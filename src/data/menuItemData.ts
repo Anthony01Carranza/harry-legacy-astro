b238d64fb01dc30746b592bca68a7ba6b31abbb9
anthony <anthonyalexandercr1992@gmail.com>
2023-06-25 13:39:54 +0200

    first commit

---
+++ src/data/menuItemData.ts
@@ -0,0 +1,21 @@
+import  {MenuItem} from "../types/types";
+
+const menuItems: MenuItem[] = [
+    {
+        title: 'Home',
+        url: '/',
+    },
+    {
+        title: 'FAQ',
+        url: '/faq',
+    },
+    {
+        title: 'Blog',
+        url: '/blog',
+    },
+
+];
+
+
+
+export default menuItems;
